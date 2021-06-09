32aff7e8aa98cfb6e34b9ab32357a6bc196ecfaf
Igor Chernyshkov <chernyshkovigor@yandex.ru>
2021-06-09 13:25:06 +0600

    profile and single pages

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 
 urlpatterns = [
+    path('recipe/<int:id>/', views.recipe_view, name='recipe'),
+    path('profile/<str:username>/<slug:tag>/', views.profile, name='tag_profile'),
+    path('profile/<str:username>/', views.profile, name='profile'),
     path('<slug:tag>/', views.index, name='tag_index'),
     path('', views.index, name='index'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 
-from .models import Recipe
+from .models import Recipe, User
 
 
 def index(request, tag=None):
@@ -18,3 +18,29 @@ def index(request, tag=None):
          'index.html',
          {'page': page, 'paginator': paginator}
     )
+
+
+def profile(request, username, tag=None):
+    author = get_object_or_404(User, username=username)
+    if tag:
+        recipes = author.recipes.filter(tag__name=str(tag))
+    else:
+        recipes = author.recipes.all()
+    paginator = Paginator(recipes, 6)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'profile.html',
+        {
+            'page': page,
+            'paginator': paginator,
+            'author': author,
+        }
+    )
+
+
+def recipe_view(request, id):
+    recipe = get_object_or_404(Recipe, id=id)
+    return render(request, 'recipe_page.html', {'recipe': recipe,})
